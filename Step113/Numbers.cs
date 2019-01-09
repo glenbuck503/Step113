6f7aa9940c78aafe785e8027998d75a71d3a5665
Glen Buck <glenbuck503@gmail.com>
2019-01-09 10:46:30 -0800

    add Step 113

---
+++ Step113/Numbers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step113
+{
+    public class Numbers
+    {
+        public void Addition(int firstNumber, int SecondNumber)
+        {
+            int firstNumberplus = firstNumber + 5;
+            firstNumber = firstNumberplus;
+           
+            Console.WriteLine(SecondNumber);
+            Console.ReadLine();
+            
+           
+        }
+        
+            
+            
+
+
+    }
+}
---
+++ Step113/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step113
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Please choose a number");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please choose a number");
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+            Numbers newNumber = new Numbers();
+            newNumber.Addition(firstNumber, secondNumber);
+            Numbers newNumber1 = new Numbers();
+            newNumber.Addition(firstNumber:firstNumber,SecondNumber:secondNumber);
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
